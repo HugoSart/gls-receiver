b05c626c536a1960517a048c4a3d607c815a8f4a
Hugo Sartori <hugo_sart@hotmail.com>
2019-03-13 03:20:06 -0300

    Código refatorado

--- src/main/java/com/hugovs/gls/DateUtils.java
+++ src/main/java/com/hugovs/gls/DateUtils.java
@@ -3,14 +3,24 @@ package com.hugovs.gls;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Date utilities.
+ */
 public class DateUtils {
 
     private DateUtils() {
         //no instance
     }
 
+    /**
+     * The default data format definition of this application.
+     */
     public static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
 
+    /**
+     * Get the current system timestamp.
+     * @return the current system timestamp.
+     */
     public static long timestamp() {
         return new Date().getTime();
     }
--- src/main/java/com/hugovs/gls/StringUtils.java
+++ src/main/java/com/hugovs/gls/StringUtils.java
@@ -1,20 +1,31 @@
 package com.hugovs.gls;
 
-import java.text.DateFormat;
-import java.util.Calendar;
-
+/**
+ * String utilities.
+ */
 public class StringUtils {
 
     private StringUtils() {
         //no instance
     }
 
-    public static String from(byte[] buffer) {
+    /**
+     * The hexadecimal representation of each number in a {@link String} format.
+     */
+    private static final String HEXES = "0123456789ABCDEF";
+
+    /**
+     * Generate a hexadecimal {@link String} of a byte array, separated with spaces.
+     *
+     * @param bytes the byte array to be converted to an hexadecimal {@link String}.
+     * @return an hexadecimal {@link String} separated with spaces.
+     */
+    public static String from(byte[] bytes) {
         StringBuilder prt = new StringBuilder("[");
         int i = 0;
-        for (byte b : buffer) {
+        for (byte b : bytes) {
             prt.append(getHex(new byte[]{b}));
-            if (i < buffer.length - 1)
+            if (i < bytes.length - 1)
                 prt.append(" ");
             i++;
         }
@@ -22,13 +33,18 @@ public class StringUtils {
         return prt.toString();
     }
 
-    static final String HEXES = "0123456789ABCDEF";
-    private static String getHex( byte [] raw ) {
-        if ( raw == null ) {
+    /**
+     * Get the hexadecimal {@link String} representation of a byte array.
+     *
+     * @param bytes the byte array to be converted to an single hexadecimal {@link String}.
+     * @return the hexadecimal representation in {@link String} of the {@code bytes}.
+     */
+    private static String getHex(byte[] bytes ) {
+        if ( bytes == null ) {
             return null;
         }
-        final StringBuilder hex = new StringBuilder( 2 * raw.length );
-        for ( final byte b : raw ) {
+        final StringBuilder hex = new StringBuilder( 2 * bytes.length );
+        for ( final byte b : bytes ) {
             hex.append(HEXES.charAt((b & 0xF0) >> 4))
                     .append(HEXES.charAt((b & 0x0F)));
         }
